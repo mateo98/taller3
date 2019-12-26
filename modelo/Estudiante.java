3f6c8fc58047b78cfc65991900e757dce2493943
Mateo Grisales Posada <magrisales@personalsoft.com.co>
2019-12-26 14:20:31 -0500

    Registrar estudiante con el constructor

--- modelo/Estudiante.java
+++ modelo/Estudiante.java
@@ -1,5 +1,20 @@
 package modelo;
 
 public class Estudiante {
-    
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private Curso curso1;
+    private Curso curso2;
+    private Curso curso3;
+    private Curso curso4;
+    private Curso curso5;
+
+    public Estudiante(int codigo, String nombre, String apellido) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+   
 }
